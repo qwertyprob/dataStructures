7ab9e2231e5302340463166c9db3035ad1a019c3
arsenii-main <eqspertars@gmail.com>
2025-07-24 16:36:24 +0300

    Add some code

---
+++ DataStructures/DynamicArray.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures;
+
+class DynamicArray<T> : IEnumerable<T>
+{
+    private int count = 0;
+    private T[] array;
+    public int Count { get { return count; } }
+    public T this[int index] 
+    {
+        get => index >= 0 &&
+               index < count ? array[index] :
+        throw new IndexOutOfRangeException("Index out of range");
+        set
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException("Index out of range");
+            }
+            array[index] = value;
+        }
+    }
+    public DynamicArray(int initialSize = 4)
+    {
+        array = new T[initialSize];
+    }
+    public void Add(T item)
+    {
+        if (count >= array.Length)
+        {
+            this.Resize();
+        }
+        array[count] = item;
+        this.count++;
+    }
+    private void Resize()
+    {
+        T[] newArray = new T[this.count * 2];
+        for (int i = 0; i < count; i++)
+        {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+    }
+    public void RemoveAt(int index)
+    {
+        if(index < 0 || index >= count)
+        {
+            throw new IndexOutOfRangeException("Index out of range");
+        }
+
+        for (int i = index; i < count - 1; i++)
+        {
+            array[i] = array[i + 1];
+        }
+
+        count--;
+    }
+    public void RemoveAtBySwap(int index)
+    {
+        if (index < 0 || index >= count)
+        {
+            throw new IndexOutOfRangeException("Index out of range");
+        }
+
+        array[index] = array[count - 1];
+
+        count--;
+    }
+    public void Insert(int index, T value)
+    {
+        if(index < 0 || index > count)
+        {
+            throw new IndexOutOfRangeException("Index out of range");
+        }
+
+        this.Add(value);
+
+        for(int i = count - 1; i > index; i--)
+        {
+            array[i] = array[i - 1];    
+
+        }
+
+        array[index] = value;
+
+
+    }
+    public void Clear()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            array[i] = default(T); // Delete refs
+        }
+
+        count = 0;
+    }
+    public override string ToString()
+    {
+
+        return "[ " + string.Join(", ", array) + " ]";
+
+
+
+    }
+    //Iterator for LINQ or foreach
+    public IEnumerator<T> GetEnumerator()
+    {
+    for (int i = 0; i < Count; i++)
+    {
+        yield return array[i];
+    }
+    }
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+
+}
+
+
---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures;
+public class Node<T>
+{
+    public T? Value { get; set; }
+    public Node<T>? NextNode { get; set; }
+    public Node(T? value)
+    {
+        Value = value;
+    }
+}
+
+public class LinkedList<T> : IEnumerable
+{
+    private int count = 0;
+    public int Count { get => this.count; }
+    private Node<T>? head = null;
+    public T this[int index]
+    {
+        get
+        {
+            if(index < 0 || index>=count)
+                throw new IndexOutOfRangeException();
+
+            int i = 0;
+            var current = head;
+            while(current != null)
+            {
+                if(i == index)
+                {
+                    return current.Value;
+                }
+                current = current.NextNode;
+                i++;
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+        set
+        {
+            if (index < 0 || index >= count)
+                throw new IndexOutOfRangeException();
+
+            int i = 0;
+            var current = head;
+            while (current != null)
+            {
+                if (i == index)
+                {
+                    current.Value = value;
+                }
+                current = current.NextNode;
+                i++;
+            }
+        }
+    }
+    public void Add(T value)
+    {
+        var newNode = new Node<T>(value);
+        if (head == null)
+        {
+            head = newNode;
+            count++;
+            return;
+
+        }
+
+        var currentNode = head;
+
+        while(currentNode.NextNode != null)
+        {
+            currentNode = currentNode.NextNode;
+        }
+
+        currentNode.NextNode = newNode;
+        count++;
+        
+    }
+    public Node<T> GetNode(int index)
+    {
+        if (index < 0 || index >= count)
+            throw new IndexOutOfRangeException();
+        int i = 0;
+        var current = head;
+
+        while (current != null) 
+        {
+            if (i == index)
+            {
+                return current;
+            }
+
+            current = current.NextNode;
+            i++;
+            
+        }
+
+        throw new IndexOutOfRangeException();
+
+    }
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= count)
+            throw new IndexOutOfRangeException();
+
+        if(index == 0)
+        {
+            head = head?.NextNode;
+            count--;
+            return;
+        }
+
+        var item = GetNode(index - 1);
+       
+
+        item.NextNode = item.NextNode.NextNode;
+        count--;
+
+
+    }
+    public void InsertAt(int index, T value)
+    {
+        if (index < 0 || index >= count)
+            throw new IndexOutOfRangeException();
+
+        var newNode = new Node<T>(value);
+
+        if(index == 0)
+        {
+            newNode.NextNode = head;
+            head = newNode;
+            count++;
+            return;
+            
+        }
+
+        var node = GetNode(index - 1);
+
+        newNode.NextNode = node.NextNode;
+        node.NextNode = newNode;
+        count++;
+
+    }
+    //Iterator for LINQ or foreach
+    public IEnumerator<T> GetEnumerator()
+    {
+        var current = head;
+        while (current != null)
+        {
+            yield return current.Value;
+            current = current.NextNode;
+        }
+    }
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+}
+
